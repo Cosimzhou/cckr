2f9e3f8a3b7c7f9e553949c310a8a0a0fd86dc02
cosimzhou <cosimzhou@hotmail.com>
2016-09-30 17:32:02 +0800

    cckr: format improve

--- src/cckr.h
+++ src/cckr.h
@@ -8,6 +8,9 @@
 
 #ifndef __CCKR_H__
 #define __CCKR_H__
+#   ifdef __cplusplus
+extern "C" {
+#   endif /* __cplusplus */
 
 #define CCKR_NUM    121
 #define CCKR_LEN    (CCKR_NUM+1)
@@ -15,5 +18,8 @@
 #include "cckr-const.h"
 #include "cckr-common.h"
 #include "cckr-evaluate.h"
-
+    
+#   ifdef __cplusplus
+};
+#   endif /* __cplusplus */
 #endif /* __CCKR_H__ */
